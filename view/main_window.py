3433123f30db889eecd58a03bd4ecb559c51a1cd
unknown <bachhoanglong28@gmail.com>
2025-11-28 22:43:23 +0800

    Add visuzlize_img on qlable

--- view/main_window.py
+++ view/main_window.py
@@ -1,6 +1,7 @@
 # view/main_window.py
 from PyQt6 import QtWidgets
 from PyQt6.uic import loadUi
+from PyQt6 import QtGui, QtCore
 import os
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -22,6 +23,49 @@ class MainWindow(QtWidgets.QMainWindow):
             te.append(text)
         else:
             print(text)
+
+    def visualize_image(self, image, q_label):
+        if image is None:
+            return
+
+        if len(image.shape) == 2:
+            h, w = image.shape
+            q_img = QtGui.QImage(
+                image.data,
+                w,
+                h,
+                w,
+                QtGui.QImage.Format.Format_Grayscale8
+            )
+
+        elif len(image.shape) == 3:
+            # Convert BGR → RGB
+            rgb = image[:, :, ::-1] 
+            h, w, ch = rgb.shape
+            bytes_per_line = ch * w
+
+            q_img = QtGui.QImage(
+                rgb.data,
+                w,
+                h,
+                bytes_per_line,
+                QtGui.QImage.Format.Format_RGB888
+            )
+
+        else:
+            self.show_error("Unsupported image format:", image.shape)
+            return
+
+        pixmap = QtGui.QPixmap.fromImage(q_img)
+
+        pixmap = pixmap.scaled(
+            q_label.width(),
+            q_label.height(),
+            QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+            QtCore.Qt.TransformationMode.SmoothTransformation
+        )
+
+        q_label.setPixmap(pixmap)
 
     def show_error(self, msg: str):
         QtWidgets.QMessageBox.critical(self, "Error", msg)
